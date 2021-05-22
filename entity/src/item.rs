dec93ed6847cfd0fcafcc0691df8c4d35fa366db
Markus Kohlhase <mail@markus-kohlhase.de>
2021-05-22 18:53:31 +0200

    Add links to the topic entity IDs

--- entity/src/item.rs
+++ entity/src/item.rs
@@ -1,22 +1,8 @@
-// NOTE:
-// The ID of this entity is not defined here.
-//
-// TODO:
-// How to express relations between entities?
-// Should we add a generic ID?
-//
-//     pub struct Item<I> {
-//         pub id: I,
-//         pub title: String
-//     }
-//
-// What would be the disadvantage of defining a concrete
-// type within the entity circle?
-//
-//     pub struct ItemId(u64);
-//
-// Or should the relation expressed in the domain,
-// with a separate type e.g. like `ItemFooRelation`?
+// The ID of this entity is not defined here
+// because an artificial ID would leaky abstraction.
+// Further readings:
+// - https://enterprisecraftsmanship.com/posts/dont-use-ids-domain-entities/
+// - https://enterprisecraftsmanship.com/posts/link-to-an-aggregate-reference-or-id/
 #[derive(Debug)]
 pub struct Item {
     pub title: String,
